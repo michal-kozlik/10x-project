22b668f93dc720f9898235bf4e36b937b94986cf
michalkozlik1-admin <michal_kozlik@poczta.onet.pl>
2025-11-01 16:32:34 +0100

    refactor: remove unused usings

--- backend/Repositories/SupabaseDiagramRepository.cs
+++ backend/Repositories/SupabaseDiagramRepository.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Npgsql;
+using System.Data;
 
 namespace SudokuApi.Repositories
 {
